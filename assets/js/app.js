df056a752533f4517eabd87dedecf6c6c9cf3ac3
Adam Grise <adamwgrise@gmail.com>
2019-05-28 20:45:07 -0500

    Updates to js

--- assets/js/app.js
+++ assets/js/app.js
@@ -28,7 +28,7 @@ var nasaImagesQuery = function () {
 
 	// The main function for the site
 	var getSearchTerm = function () {
-		if ($('#search-term') === '') {
+		if ($('#search-term').val().trim() === '') {
 			searchTerm = ''
 		} else {
 			searchTerm = $('#search-term').val().trim();
@@ -36,37 +36,48 @@ var nasaImagesQuery = function () {
 	};
 
 	var getYearStart = function () {
-		if ($('#search-start-date') === '') {
-			yearStart = '1900';
+		if ($('#search-start-date').val().trim() === '') {
+			yearStart = '';
 		} else {
-			yearStart = $('#search-start-date').val().trim();
+			yearStart = '&year_start=' + $('#search-start-date').val().trim();
 		};
 	};
 
 	var getYearEnd = function () {
-		if ($('#search-end-date') === '') {
-			yearEnd = '2199';
+		if ($('#search-end-date').val().trim() === '') {
+			yearEnd = '';
 		} else {
-			yearEnd = $('#search-end-date').val().trim();
+			yearEnd = '&year_end=' + $('#search-end-date').val().trim();
 		};
 	};
 
 	var getResultsQty = function () {
-		if ($('#resultsQty') === '') {
+		if ($('#resultsQty').val().trim() === '') {
 			resultsQty = 10
 		} else {
 			resultsQty = $('#resultsQty').val().trim();
 		};
 	};
 
+
 	getSearchTerm();
 	getYearStart();
 	getYearEnd();
 	getResultsQty();
 
+	if (searchTerm === '' && yearStart === '' && yearEnd === '') {
+		$('#errorMessage').show();
+		return;
+	} else{
+		$('#errorMessage').hide();
+	}
+
+	queryError();
+
 	console.log('Query values: ' + searchTerm + ', ' + yearStart + '-' + yearEnd + '; ' + resultsQty + ' results desired');
 
-	nasaQueryURL = 'https://images-api.nasa.gov/search?q=' + searchTerm + mediaType + '&year_start=' + yearStart + '&year_end=' + yearEnd
+	nasaQueryURL = 'https://images-api.nasa.gov/search?q=' + searchTerm + mediaType + yearStart + yearEnd;
+
 	mediaType = function () {
 		if ($('#media-type') === '') {
 			return ''
@@ -89,14 +100,20 @@ var nasaImagesQuery = function () {
 			resImg.attr({
 				'class': 'resImg',
 				src: resultsArr[i].links[0].href, // Just the thumbnail
-				// Also add an actual link to it for full size
+				// TO DO Also add an actual link to it for full size
 				'title': resultsArr[i].data[0].title,
 			});
 			// resBox.wrap('<a href=' + results.Arr[i].href + '></a>')
+
 			console.log(resultsArr[i].href);
-			for (j = 0 ; j < 5 ; j++) {
-				console.log(resultsArr[i].href[j]);
-			}
+			console.log(resultsArr[i].href.length);
+			console.log(resultsArr[i].href.length);
+			// var myJSON = JSON.parse(resultsArr[i].href);
+			// console.log(myJSON);
+			// for (j = 0 ; j < 5 ; j++) {
+			// 	console.log(resultsArr[i].href[j]);
+			// }
+
 			$('#results').prepend(resBox);
 			resBox.append(resImg);
 			resBox.append(resultsArr[i].data[0].title);
